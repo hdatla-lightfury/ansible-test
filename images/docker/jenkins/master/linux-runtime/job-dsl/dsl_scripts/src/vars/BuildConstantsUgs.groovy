a49fc86352fb281a78867992045d820cb784ecda
hdatla-lightfury <harish@lightfurygames.com>
2025-07-07 15:08:55 +0530

    Delete images/docker/jenkins directory

--- images/docker/jenkins/master/linux-runtime/job-dsl/dsl_scripts/src/vars/BuildConstantsUgs.groovy
+++
@@ -1,9 +0,0 @@
-package vars
-class BuildConstantsUgs {
-    static BUILD_CONFIG_VALUES = ['ALL', 'Development']
-    static BUILD_TARGET_VALUES = ['ALL', 'TitanEditor']
-    static STREAMS = [
-                        "ALL",
-                        "//titan-game/development"
-                  ]
-}
